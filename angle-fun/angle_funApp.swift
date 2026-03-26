be44e86ed1c6374b77b1fa1f1154191511b3cbfa
Vlad <vladtv2374@yandex.ru>
2026-03-26 14:11:10 +0300

    Initial Commit

---
+++ angle-fun/angle_funApp.swift
@@ -0,0 +1,17 @@
+//
+//  angle_funApp.swift
+//  angle-fun
+//
+//  Created by Владислав Туровец on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct angle_funApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
